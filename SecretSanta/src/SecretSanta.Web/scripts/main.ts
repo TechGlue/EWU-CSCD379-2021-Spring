eaf90301ec5c707e1f13295f68a82c3f03af5ec2
Luis Garcia <garcialuis24@hotmail.com>
2021-05-16 22:27:07 -0700

    Final commit for the night started on the alpine.js conversion still work in progress

--- SecretSanta/src/SecretSanta.Web/scripts/main.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/main.ts
@@ -1,7 +1,7 @@
 import '../styles/site.css';
 
 import 'alpinejs';
-
+import axios from "axios";
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
@@ -9,6 +9,12 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 
 library.add(fas, far, fab);
 dom.watch();
+
+interface User{
+    id:number,
+    firstName: string,
+    lastName: string
+}
 
 export function setupNav() {
     return {
@@ -20,6 +26,29 @@ export function setupNav() {
                 } else {
                     headerNav.classList.add('hidden');
                 }
+            }
+        }
+    }
+}
+
+export function setupUsers() {
+    return {
+        users: [] as User[],
+        async mounted() {
+            await this.loadUsers();
+        },
+        async deleteUser(currentUser: User) {
+            if (confirm(`Are you sure you want to delete ${currentUser.firstName} ${currentUser.lastName}?`)) {
+                await axios.delete(`https://localhost:5101/api/users/${currentUser.id}`);
+                await this.loadUsers();
+            }
+        },
+        async loadUsers() {
+            try {
+                const response = await axios.get("https://localhost:5101/api/users");
+                this.users = response.data;
+            } catch (error) {
+                console.log(error);
             }
         }
     }
