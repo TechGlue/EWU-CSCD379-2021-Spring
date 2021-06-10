b01cfed6a70cf16a23dc19d57da722d2c857c97c
Luis Garcia <lgarcia29@eagles.ewu.edu>
2021-06-09 19:58:21 -0700

    Pulling from database for users all of a sudden but working perfect :(

--- SecretSanta/src/SecretSanta.Api/Program.cs
+++ SecretSanta/src/SecretSanta.Api/Program.cs
@@ -16,23 +16,24 @@ namespace SecretSanta.Api
         public static void Main(string[] args)
         {
             //database testing stuff.
-            using (var dbcontext = new DbContext())
-            {
-                // dbcontext.Users.Add(new User() {UserId = 9, FirstName = "Jef", LastName = "g"});
-                // dbcontext.Groups.Add(new Group() {GroupId = 42, Name = "Mikes Pizza"});
-                // dbcontext.SaveChanges();
-                foreach (var user in dbcontext.Users.ToList())
-                {
-                    Console.WriteLine($"First= {user.FirstName}, Last = {user.LastName}");
-                }
-            
-                foreach (var group in dbcontext.Groups.ToList())
-                {
-                    Console.WriteLine($"GroupId= {group.GroupId}, GroupName  = {group.Name}"); 
-                    
-                }
-                Console.ReadLine();
-            }
+            // using (var dbcontext = new DbContext())
+            // {
+            //     // // // dbcontext.Users.Add(new User() {UserId = 9, FirstName = "Jef", LastName = "g"});
+            //     // // // dbcontext.Groups.Add(new Group() {GroupId = 42, Name = "Mikes Pizza"});
+            //     // // // dbcontext.SaveChanges();
+            //     // foreach (var user in dbcontext.Users.ToList())
+            //     // {
+            //     //     Console.WriteLine($"First= {user.FirstName}, Last = {user.LastName}");
+            //     // }
+            //     //
+            //     // foreach (var group in dbcontext.Groups.ToList())
+            //     // {
+            //     //     Console.WriteLine($"GroupId= {group.GroupId}, GroupName  = {group.Name}"); 
+            //     //     
+            //     // }
+            //     Console.ReadLine();
+            // }
+            //
             
             CreateHostBuilder(args).Build().Run(); 
         }
