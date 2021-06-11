40f345da969f2b7c71990ef817afc8553fa366c2
Pedro H <phernandez5@eagles.ewu.edu>
2021-06-10 21:41:03 -0700

    defaulting db.log & main.db, setting up database category

--- SecretSanta/src/SecretSanta.Api/Startup.cs
+++ SecretSanta/src/SecretSanta.Api/Startup.cs
@@ -3,6 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SecretSanta.Business;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using SecretSanta.Data;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using DbContext = SecretSanta.Data.DbContext;
+using Microsoft.AspNetCore.Builder;
 
 namespace SecretSanta.Api
 {
@@ -10,8 +18,24 @@ namespace SecretSanta.Api
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+            => Configuration = config;
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(builder => {
+                builder.AddSerilog(logger: Log.Logger);
+            });
+            services.AddDbContext<DbContext>( options =>
+            {
+                options.UseSqlite($"Data Source={Configuration.GetConnectionString("DbConnection")}");
+                options.UseLoggerFactory(LoggerFactory.Create( builder => 
+                        builder.AddSerilog(Log.Logger.ForContext<DbContext>().ForContext("Category", "Database"))
+                ));
+            });
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IGroupRepository, GroupRepository>();
             services.AddScoped<IGiftRepository, GiftRepository>();
@@ -32,6 +56,7 @@ namespace SecretSanta.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSerilogRequestLogging();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
